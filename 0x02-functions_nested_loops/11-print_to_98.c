d4ff926f9b860b2e0dbfbb867e34c4adbc58c9c0
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2022-12-14 20:45:26 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,21 +12,21 @@
 
 void print_to_98(int n)
 {
-  if (n < 98)
-    {
-      while (n < 98)
+	if (n < 98)
 	{
-	  printf("%d, ", n);
-	  n++;
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
-    }
-  else if (n > 98)
-    {
-      while (n > 98)
+	else if (n > 98)
 	{
-	  printf("%d, ", n);
-	  n--;
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
-    }
-  printf("98\n");
+	printf("98\n");
 }
