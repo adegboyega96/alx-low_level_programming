7fa8fd1c51afdb4dee8d53b4518410259b31cb5b
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2022-12-27 19:17:11 -0800

    program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-  int i = 97;
+	int i = 97;
 
-  while (i < 123)
-    {
-      if (i != 101 && i != 113)
+	while (i < 123)
 	{
-	  putchar(i);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
 	}
-      i++;
-    }
-  putchar(10);
+	putchar(10);
 
-  return (0);
+	return (0);
 }
