b296471151a0c9363e799e9c9f42daf0618acfe3
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2022-12-27 19:20:24 -0800

    program that prints the lowercase alphabet in reverse, followed by a new line.

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-  int i = 122;
+	int i = 122;
 
-  while (i > 96)
-    {
-      putchar(i);
-      i--;
-    }
-  putchar(10);
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
 
-  return (0);
+	return (0);
 }
