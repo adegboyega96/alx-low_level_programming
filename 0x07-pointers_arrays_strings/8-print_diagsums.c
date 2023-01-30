793330c7fdfca775f78b4359ed673353a9eaa80d
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2023-01-29 20:43:18 -0800

    update task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,34 +4,17 @@
 /**
  * print_diagsums - prints the sums of the two diagonals
  * @a: array
- * @size: size of array
- * Return: void
+ *  @size: size of array
+ *  Return: Always 0 (Success)
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int bdiagsum = 0;
-	int fdiagsum = 0;
-	int prev = 0;
+	int i, n, total1 = 0, total2 = 0;
 
-	for (i = 0; i < size * size; i++)
-	{
-		if (i == 0)
-		{
-			bdiagsum += *(a + i);
-			prev = i;
-		}
-		else if (i == (prev + size + 1))
-		{
-			bdiagsum += *(a + i);
-			prev = i;
-		}
-	}
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		total1 = total1 + a[i];
 
-	for (i = 0; i < size; i++)
-	{
-		fdiagsum += *(a + (size * (i + 1) - (i + 1)));
-	}
-	printf("%d, %d\n", bdiagsum, fdiagsum);
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		total2 = total2 + a[n];
+	printf("%d, %d\n", total1, total2);
 }
